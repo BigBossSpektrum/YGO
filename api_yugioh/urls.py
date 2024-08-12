009f0e275e56ed93dd07dc28ed16f46f83f75972
BigBossSpektrum <silvekerhernandez@proton.me>
2024-08-12 10:41:01 -0500

    api update

--- api_yugioh/urls.py
+++ api_yugioh/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('card/', views.get_card, name='get_card'),
-    path('card/<str:card_name>/', views.get_card, name='get_card'),
-    path('search/', views.search_card, name='search_card'),
+    # path('card/', views.get_card, name='get_card'),
+    # path('card/<str:card_name>/', views.get_card, name='get_card'),
+    # path('search/', views.search_card, name='search_card'),
     path('', views.card_info_view, name='card_info_view'),
 ]
--- api_yugioh/views.py
+++ api_yugioh/views.py
@@ -5,40 +5,22 @@ from django.shortcuts import render
 api_url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
 # api_url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Decode%20Talker'
 
+
 def card_info_view(request):
+    
+    # Realizar la solicitud a la API
     response = requests.get(api_url)
     
     if response.status_code == 200:
-        data = response.json()
-        cards = random.sample(data['data'], 50)  # Aquí obtienes la lista de cartas de la API
+        # Si la solicitud es exitosa, obtenemos las cartas
+        cards = response.json()['data']
+        
+        # Selecciona 50 cartas al azar
+        random_cards = random.sample(cards, 50)
+        
+        # Pasar las cartas a la plantilla
+        context = {'cards': random_cards}
+        return render(request, 'base.html', context)
     else:
-        cards = []  # En caso de que haya un error en la API
-
-    return render(request, 'base.html', {'cards': cards})
-
-
-
-# def get_cards(request):
-#     response = requests.get(api_url)
-#     data = response.json()
-
-#     cards = random.sample(data['data'], 50) #obtener solo los primeros 10 elementos
-#     return render (request, 'cards_info.html', {'cards': cards})
-
-# def get_card(request, card_name):
-#     response = requests.get(f'{api_url}?name={card_name}')
-#     card = response.json().get('data', [])[0]
-#     return render(request, 'card_info.html', {'card': card})
-
-# def get_card_random(request, random='data=random'):
-#     response = requests.get(f'{api_url}?{random}')
-#     card = response.json().get('data', [])[0]
-#     card_image_url = card.get('card_images', [])[0].get('image_url')
-#     return render(request, './templates/base.html', {'card': card, 'card_image_url': card_image_url})
-
-# def search_card(request):
-#     query = request.GET.get('q')
-#     response = requests.get(f'{api_url}?name={query}')
-#     cards = response.json().get('data', [])
-#     return render(request, 'search.html', {'cards': cards})
-
+        # En caso de error, puedes manejarlo aquí
+        return render(request, 'base.html', {'error': 'No se pudieron obtener las cartas de la API'})
