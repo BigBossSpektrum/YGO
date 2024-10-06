660417494daa52b1253cba534a9538be6a2c74d5
BigBossSpektrum <silvekerhernandez@proton.me>
2024-10-06 00:44:12 -0500

    Inicializando proyecto Djangov6

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yugioh_page_main.settings')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,23 +15,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    # Obtenemos el puerto desde la variable de entorno o usamos 4000 como predeterminado
+    port = os.environ.get("PORT", "4000")
+
+    # Ejecutamos el comando para correr el servidor en el puerto asignado
+    execute_from_command_line(sys.argv + ["runserver", f"0.0.0.0:{port}"])
 
 if __name__ == '__main__':
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-
-    # Modifica esta línea para utilizar el puerto asignado por Render
-    port = os.environ.get("PORT", "4000")
-    execute_from_command_line(sys.argv + ["runserver", "0.0.0.0:" + port])
-
     main()
