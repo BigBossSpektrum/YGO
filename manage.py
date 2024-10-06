c0d3a4888e748ae4f969e041a5aa0da3fdea52a4
BigBossSpektrum <silvekerhernandez@proton.me>
2024-10-06 00:20:19 -0500

    Inicializando proyecto Djangov3

--- manage.py
+++ manage.py
@@ -19,4 +19,19 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    # Modifica esta línea para utilizar el puerto asignado por Render
+    port = os.environ.get("PORT", "8000")
+    execute_from_command_line(sys.argv + ["runserver", "0.0.0.0:" + port])
+
+    #main()
