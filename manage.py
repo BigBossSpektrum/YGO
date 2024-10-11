aaba574b670c2c9a1786890bdb9e1f21b9f315a2
BigBossSpektrum <silvekerhernandez@proton.me>
2024-10-10 21:39:57 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,11 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yugioh_page_main.settings')
-    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,12 +13,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    # Obtenemos el puerto desde la variable de entorno o usamos 4000 como predeterminado
-    port = os.environ.get("PORT", "4000")
-
-    # Ejecutamos el comando para correr el servidor en el puerto asignado
-    execute_from_command_line(sys.argv + ["runserver", f"0.0.0.0:{port}"])
-
+    execute_from_command_line(sys.argv)
 if __name__ == '__main__':
     main()
