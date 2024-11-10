7a27763c83843e50bae3068bf581316a3086bc1c
Bigboss <silvekerhernandez@proton.me>
2024-11-09 21:49:45 -0500

    added a new filter on randomcard page

--- api_yugioh/models.py
+++ api_yugioh/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class login(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
